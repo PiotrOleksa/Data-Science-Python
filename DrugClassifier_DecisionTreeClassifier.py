40c0eaa966b6a7ce80da4aad456fa816ffdbfc5e
PiotrOleksa <79694884+PiotrOleksa@users.noreply.github.com>
2021-02-26 11:13:56 +0100

    Update and rename DecisionTreeClassifier.py to DrugClassifier_DecisionTreeClassifier.py

--- DrugClassifier_DecisionTreeClassifier.py
+++ DrugClassifier_DecisionTreeClassifier.py
@@ -16,37 +16,37 @@ df = pd.read_csv(r'C:\Users\Piotr\Desktop\Projekty Python\5. Decision Tree\drug2
 
 #---------------------- Data Exploring
 #Describe
-#pd.set_option('float_format', '{:.2f}'.format)
-#sub_plot = plt.subplot(111, frame_on=False)
+pd.set_option('float_format', '{:.2f}'.format)
+sub_plot = plt.subplot(111, frame_on=False)
 
-#sub_plot.xaxis.set_visible(False) 
-#sub_plot.yaxis.set_visible(False) 
+sub_plot.xaxis.set_visible(False) 
+sub_plot.yaxis.set_visible(False) 
 
-#table(sub_plot, df.describe(), loc='upper right').scale(1, 2)
-#plt.title("Patients Describe")
-#plt.savefig('pat_desc.png', dpi=199)
+table(sub_plot, df.describe(), loc='upper right').scale(1, 2)
+plt.title("Patients Describe")
+plt.savefig('pat_desc.png', dpi=199)
 
 #Pie charts
 #Sex
-#fig1, ax1 = plt.subplots()
-#ax1.pie(df['Sex'].value_counts(), labels=['Males', 'Females'],autopct='%1.1f%%',startangle=90)
-#ax1.axis('equal')
-#plt.title('Sex Proportions')
-#plt.savefig('sx_pie.png', dpi=199)
+fig1, ax1 = plt.subplots()
+ax1.pie(df['Sex'].value_counts(), labels=['Males', 'Females'],autopct='%1.1f%%',startangle=90)
+ax1.axis('equal')
+plt.title('Sex Proportions')
+plt.savefig('sx_pie.png', dpi=199)
 
 #BP
-#fig1, ax1 = plt.subplots()
-#ax1.pie(df['BP'].value_counts(), labels=df['BP'].unique(),autopct='%1.1f%%',startangle=90)
-#ax1.axis('equal')
-#plt.title('BP')
-#plt.savefig('bp_pie.png', dpi=199)
+fig1, ax1 = plt.subplots()
+ax1.pie(df['BP'].value_counts(), labels=df['BP'].unique(),autopct='%1.1f%%',startangle=90)
+ax1.axis('equal')
+plt.title('BP')
+plt.savefig('bp_pie.png', dpi=199)
 
 #Cholesterol
-#fig1, ax1 = plt.subplots()
-#ax1.pie(df['Cholesterol'].value_counts(), labels=df['Cholesterol'].unique(),autopct='%1.1f%%',startangle=90)
-#ax1.axis('equal')
-#plt.title('Chorestelor')
-#plt.savefig('ch_pie.png', dpi=199)
+fig1, ax1 = plt.subplots()
+ax1.pie(df['Cholesterol'].value_counts(), labels=df['Cholesterol'].unique(),autopct='%1.1f%%',startangle=90)
+ax1.axis('equal')
+plt.title('Chorestelor')
+plt.savefig('ch_pie.png', dpi=199)
 
 #Drug
 drug_count = pd.value_counts(df['Drug'].values, sort=True)
